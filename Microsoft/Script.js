3b8cb0f64af6f113456dab0a863415ca49e3d59d
BAMTEFA, Olorunshogo Moses <bamtefaolorunshogo12@gmail.com>
2024-01-15 16:11:20 +0100

    The Hamburger menu isn't working. I'll finish this slideshow after resting. I'm tired already

--- Microsoft/Script.js
+++ Microsoft/Script.js
@@ -1,2 +1,21 @@
 // The button helps to get back to the top of the page
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+// This block is for the slideshow
+{
+    let slideIndex =1;
+    showSlides(slideIndex);
+
+    // The next and previous controls
+    function plusSlides (n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName('.showcase fade');
+        let controls = document.getElementsByClassName('fas fa-chevron-left', 'fas fa-chevron-right');
+        if (n > slides.length) {slideIndex = 1}
+        if (n < slides.length) {slideIndex = slides.length}
+    }
+}
